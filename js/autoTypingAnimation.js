e6bf82c6ebf360a189e602e37186f6ac9ff087ab
Pragya Sharma <156325296+Pragya2505@users.noreply.github.com>
2024-06-17 18:52:42 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Biomedical Engineer:My expertise in the areas of Biomedical Engineering allows me to approach projects with confidence and deliver high-quality results.I strongly believe in the value of continuous learning and staying adaptable in the ever-evolving field of Healthcare.I actively seek opportunities to expand my knowledge and skills, embracing new technologies and innovative approaches.."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
