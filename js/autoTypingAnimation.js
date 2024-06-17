4cae12eea028601643de1bed039b4aa0ef94cf90
Pragya Sharma <156325296+Pragya2505@users.noreply.github.com>
2024-06-17 23:06:56 +0530

    Update autoTypingAnimation.js

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>. Known for my relentless pursuit of knowledge and strong work ethic. I thrive in challenging environments and strive for excellence in everything I do. Apart from this I am both Front-end and Back-end Developer who adept in all stages of advanced web development"
+        "<span> Known for my relentless pursuit of knowledge and strong work ethic. I thrive in challenging environments and strive for excellence in everything I do. Apart from this I am both Front-end and Back-end Developer who adept in all stages of advanced web development"
       ],
       typeSpeed: 7,
       contentType: 'html'
